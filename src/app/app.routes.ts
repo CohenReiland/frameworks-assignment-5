5b7ad1a6f43b00a299d21bf4dfe8143135e0c342
Reiland, Cohen <cohen200521@gmail.com>
2026-04-12 20:18:23 -0500

    Added Routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,34 @@
 import { Routes } from '@angular/router';
+import { DashboardComponent } from './dashboard-component/dashboard-component';
+import { TransactionsComponent } from './transactions-component/transactions-component';
+import { CategoriesComponent } from './categories-component/categories-component';
+import { AuthComponent } from './auth-component/auth-component';
+import { BudgetComponent } from './budget-component/budget-component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: AuthComponent,
+    title: 'Auth',
+  },
+  {
+    path: '',
+    component: DashboardComponent,
+    title: 'Dashboard',
+  },
+  {
+    path: '',
+    component: TransactionsComponent,
+    title: 'Transactions',
+  },
+  {
+    path: '',
+    component: CategoriesComponent,
+    title: 'Categories',
+  },
+  {
+    path: '',
+    component: BudgetComponent,
+    title: 'Budget',
+  },
+];
