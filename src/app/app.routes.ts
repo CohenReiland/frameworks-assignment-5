39aaa0a16418cddb6c80a8ebdc113f8aa8387e5a
Reiland, Cohen <cohen200521@gmail.com>
2026-04-17 19:08:18 -0500

    Added Navbar with Styling

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,14 +2,20 @@ import { Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard-component/dashboard-component';
 import { TransactionsComponent } from './transactions-component/transactions-component';
 import { CategoriesComponent } from './categories-component/categories-component';
-import { AuthComponent } from './auth-component/auth-component';
 import { BudgetComponent } from './budget-component/budget-component';
+import { SignupComponent } from './signup-component/signup-component';
+import { LoginComponent } from './login-component/login-component';
 
 export const routes: Routes = [
   {
     path: '',
-    component: AuthComponent,
-    title: 'Auth',
+    component: SignupComponent,
+    title: 'Sign Up',
+  },
+  {
+    path: '',
+    component: LoginComponent,
+    title: 'Login',
   },
   {
     path: '',
