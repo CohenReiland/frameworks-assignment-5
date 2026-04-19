6ea3db2ba806abe61e3ba4adfdd8a02d1abbf836
Reiland, Cohen <cohen200521@gmail.com>
2026-04-18 20:26:51 -0500

    Added Logout Button

--- src/app/navbar-component/navbar-component.ts
+++ src/app/navbar-component/navbar-component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
-import { RouterLink, RouterLinkActive } from '@angular/router';
+import { Component, inject } from '@angular/core';
+import { Router, RouterLink, RouterLinkActive } from '@angular/router';
+import { AuthService } from '../services/auth-service';
 
 @Component({
   selector: 'app-navbar-component',
@@ -8,4 +9,12 @@ import { RouterLink, RouterLinkActive } from '@angular/router';
   templateUrl: './navbar-component.html',
   styleUrl: './navbar-component.css',
 })
-export class NavbarComponent {}
+export class NavbarComponent {
+  private readonly authService = inject(AuthService);
+  private readonly router = inject(Router);
+
+  async onLogout(): Promise<void> {
+    await this.authService.logout();
+    await this.router.navigate(['/login']);
+  }
+}
